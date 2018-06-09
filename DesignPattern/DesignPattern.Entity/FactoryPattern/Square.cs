f8bd4f5b62863abb4a55f896b9bde8a38990df6b
amuratgencay <amuratgencay@hotmail.com>
2018-06-09 19:59:39 +0300

    Factory Pattern implemented

---
+++ DesignPattern/DesignPattern.Entity/FactoryPattern/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Entity.FactoryPattern
+{
+    public class Square : Shape
+    {
+        public double Size { get; set; }
+        
+        public Square(double size)
+        {
+            Size = size;
+            
+        }
+        public override double Area()
+        {
+            return Size * Size;
+        }
+
+        public override string ToString()
+        {
+            return $"Square[{Size},{Size}] = {Area()}";
+        }
+    }
+}
