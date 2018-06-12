f9be4239b3a7b4fa2388a2e4de819d5aff24e287
amuratgencay <amuratgencay@hotmail.com>
2018-06-12 14:40:12 +0300

    Composite Pattern implemented.

---
+++ DesignPattern/DesignPattern.BLL/StructuralPatterns/CompositePattern/ProductGenerator.cs
@@ -0,0 +1,38 @@
+﻿using DesignPattern.Entity.StructuralPatterns.CompositePattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BLL.StructuralPatterns.CompositePattern
+{
+    public static class ProductGenerator
+    {
+        public static CategoryManager GenerateProducts()
+        {
+            Category computer = new Category("Computer");
+            Category laptop = new Category("Laptop");
+            Category pc = new Category("PC");
+
+            Product lenovo = new Product("Lenovo", 2599m);
+            Product hp = new Product("HP", 2399m);
+
+            Product casper = new Product("Casper", 1699m);
+            Product apple = new Product("Apple", 5499m);
+
+            CategoryManager cm = new CategoryManager();
+            cm.AddCategory(computer);
+            cm.AddTopCategory(computer, laptop);
+            cm.AddTopCategory(computer, pc);
+
+            cm.AddProduct(laptop, lenovo);
+            cm.AddProduct(laptop, hp);
+
+            cm.AddProduct(pc, casper);
+            cm.AddProduct(pc, apple);
+
+            return cm;
+        } 
+    }
+}
---
+++ DesignPattern/DesignPattern.ConsoleApp/StructuralPatterns/CompositePattern/CompositePatternExample.cs
@@ -0,0 +1,23 @@
+﻿using DesignPattern.BLL.StructuralPatterns.CompositePattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.ConsoleApp.StructuralPatterns.CompositePattern
+{
+    public static class CompositePatternExample
+    {
+        public static void Run()
+        {
+           
+            Console.WriteLine("<Composite Pattern Example>");
+            Console.WriteLine();            
+            ProductGenerator.GenerateProducts().ListCategories();
+            Console.WriteLine();
+            Console.WriteLine("</Composite Pattern Example>");
+            Console.WriteLine();
+        }
+    }
+}
