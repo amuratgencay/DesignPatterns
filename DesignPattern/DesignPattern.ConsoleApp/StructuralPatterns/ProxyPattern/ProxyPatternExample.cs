3490974cdfd8b8fa4f246896a6ed5a168b4bd4a7
amuratgencay <amuratgencay@hotmail.com>
2018-06-12 11:40:30 +0300

    Proxy Pattern implemented.

---
+++ DesignPattern/DesignPattern.ConsoleApp/StructuralPatterns/ProxyPattern/ProxyPatternExample.cs
@@ -0,0 +1,25 @@
+﻿
+using DesignPattern.BLL.StructuralPatterns.ProxyPattern;
+using DesignPattern.Entity.StructuralPatterns.ProxyPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.ConsoleApp.StructuralPatterns.FlyweightPattern
+{
+    public static class ProxyPatternExample
+    {
+        public static void Run()
+        {
+            IWriter pw = new ProxyWriter(WriterType.Console);
+            Console.WriteLine("<Proxy Pattern Example>");
+            Console.WriteLine();
+            pw.Write("\tMessage");
+            Console.WriteLine();
+            Console.WriteLine("</Proxy Pattern Example>");
+            Console.WriteLine();
+        }
+    }
+}
