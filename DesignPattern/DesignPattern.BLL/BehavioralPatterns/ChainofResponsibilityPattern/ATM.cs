11bce054e966d742371231bf16f982dcb51a5c6a
amuratgencay <amuratgencay@hotmail.com>
2018-06-13 15:35:02 +0300

    Chain of Responsibility Pattern implemented.

---
+++ DesignPattern/DesignPattern.BLL/BehavioralPatterns/ChainofResponsibilityPattern/ATM.cs
@@ -0,0 +1,58 @@
+﻿using DesignPattern.Entity.BehavioralPatterns.ChainofResponsibilityPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BLL.BehavioralPatterns.ChainofResponsibilityPattern
+{
+    public class ATM
+    {
+        private readonly Banknote banknote;
+        public ATM(List<Banknote> banknotes)
+        {
+            foreach (var item in banknotes)
+            {
+                if(banknote == null)
+                {
+                    banknote = item;
+                }
+                else
+                {
+                    banknote.AddNext(item);
+                }
+            }
+        }
+
+        private List<Banknote> GetMoney(int amount, Banknote root, ref List<Banknote> banknotes)
+        {
+            if ((root == null) || (amount == 0))
+                return banknotes;
+
+            if((root.Amount > amount) || (root.Count == 0))
+            {
+                return GetMoney(amount, root.Next, ref banknotes);
+            }
+            var money = root.GetMoney();
+            if(banknotes.Any(x=>x.Amount == money.Amount))
+            {
+                banknotes.FirstOrDefault(x => x.Amount == money.Amount)?.AddMoney(money);
+            }
+            else
+            {
+                banknotes.Add(new Banknote(money.Amount, 1));
+            }
+            
+            amount -= money.Amount;
+            return GetMoney(amount, root, ref banknotes);
+        }
+        public List<Banknote> GetMoney(int amount)
+        {
+            var banknotes = new List<Banknote>();
+            var res =  GetMoney(amount, banknote, ref banknotes);
+            return res;
+        }
+
+    }
+}
---
+++ DesignPattern/DesignPattern.ConsoleApp/BehavioralPatterns/ChainofResponsibilityPattern/ChainofResponsibilityPatternExample.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.BLL.BehavioralPatterns.ChainofResponsibilityPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.ConsoleApp.BehavioralPatterns.ChainofResponsibilityPattern
+{
+    public static class ChainofResponsibilityPatternExample
+    {
+        public static void Run()
+        {
+            var atm = new ATM(new List<Banknote> { new Banknote(200, 3), new Banknote(100, 5), new Banknote(50, 25), new Banknote(20, 30), new Banknote(10, 100), new Banknote(5, 150), new Banknote(1, 200) });
+            Console.WriteLine("<Chain of Responsibility Pattern Example>");
+            Console.WriteLine();
+            foreach (var item in atm.GetMoney(1375))
+            {
+                
+                Console.WriteLine("\t" + item.Count + "x" + item.Amount  );
+            }
+            Console.WriteLine();
+            Console.WriteLine("</Chain of Responsibility Pattern Example>");
+            Console.WriteLine();
+        }
+    }
+}
