7a44cb95d7ab3fed8321566324d55b1d83d9a9a0
amuratgencay <amuratgencay@hotmail.com>
2018-06-14 17:09:59 +0300

    Command Pattern implementation changed.

--- DesignPattern/DesignPattern.Entity/BehavioralPatterns/CommandPattern/IOperator.cs
+++ DesignPattern/DesignPattern.Entity/BehavioralPatterns/CommandPattern/IOperator.cs
@@ -1,7 +1,61 @@
 ﻿namespace DesignPattern.Entity.BehavioralPatterns.CommandPattern
 {
+    public class Number
+    {
+        public Number(int value)
+        {
+            Value = value;
+        }
+
+        public Number(double value)
+        {
+            Value = value;
+        }
+
+        public double Value { get; private set; }
+
+        public Number Sum(Number value)
+        {
+            Value += value.Value;
+            return this;
+        }
+
+        public Number Sub(Number value)
+        {
+            Value -= value.Value;
+            return this;
+        }
+
+        public Number Mul(Number value)
+        {
+            Value *= value.Value;
+            return this;
+        }
+
+        public Number Div(Number value)
+        {
+            Value /= value.Value;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return $"Number: {Value}";
+        }
+
+        public static implicit operator Number(int value)
+        {
+            return new Number(value);
+        }
+
+        public static implicit operator Number(double value)
+        {
+            return new Number(value);
+        }
+    }
+
     public interface IOperator
     {
-        double Operate(double op1, double op2);
+        Number Operate();
     }
 }
